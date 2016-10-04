f857c392ac10b0fb755a9a955c4af0e8169e2a62
ruslan <pdbq2121@gmail.com>
2016-10-04 16:07:35 +0300

    add: reducer

---
+++ blog/static/src/reducers/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ruslan on 04.10.16.
+ */
+const initialState = {
+    user: 'test user'
+};
+
+export default function userstate(state = initialState){
+    return state;
+}
+
+/*
+Note:
+    ? function userstate()
+
+ */
